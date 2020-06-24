190ffb83351ac98e272910bc59cd9a21a086927f
Pepm17 <pablopupo1797@gmail.com>
2020-06-23 21:17:33 -0500

    Add movie complete

--- client/src/app/components/header/add-movie-modal/add-movie-modal.component.ts
+++ client/src/app/components/header/add-movie-modal/add-movie-modal.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from 'src/app/models/Movie';
+import { MoviesService } from '../../../services/movies.service'
 
 @Component({
   selector: 'app-add-movie-modal',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddMovieModalComponent implements OnInit {
 
-  constructor() { }
+  movie: Movie = {
+    id: 0,
+    title: '',
+    description: '',
+    score: 0,
+    image: ''
+  };
+  constructor(private movieService: MoviesService) { }
 
   ngOnInit(): void {
+  }
+
+  insertMovie(){
+
+    this.movieService.insertMovie(this.movie).subscribe(
+      res => {
+        console.log(res);
+        window.location.reload();
+      },
+      err => console.log(err)
+    )
+
+  }
+
+  onKey(event: any) { // without type info
+    console.log(this.movie)
+    this.movie.image += event.target.value;
   }
 
 }
